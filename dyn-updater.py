aed2176f24ebbc449df29400b8a12899166b5a44
Eduardo Cataño Conde <socketz@gmail.com>
2017-03-04 15:06:20 +0000

    Updated script

--- dyn-updater.py
+++ dyn-updater.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+
+import json, ovh, string, re
+
+params = {
+	"zone_name": "socketz.net",
+	"subdomain": "home-vpn",
+	"id": ""
+}
+
+
+# create a client
+client = ovh.Client(config_file='ovh.conf')
+
+# /domain/zone/{zoneName}/dynHost/login/{login}
+
+result = json.loads(client.get('/domain/zone/'+params['zone_name']+'/dynHost/record', subDomain=params['subdomain']))
+params['id'] = result[0]
+
+result = client.get('/domain/zone/'+params['zone_name']+'/dynHost/record/'+params['id'])
+
+print json.dumps(result, indent=4)
+
